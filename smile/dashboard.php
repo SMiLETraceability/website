b9e6c3992c64ad76a789c1e1fe95eed610f2d2fc
epignotti <epignott@oa-edu-172-44.wireless.abdn.ac.uk>
2014-05-21 16:23:26 +0100

    Updated Dashboard

--- smile/dashboard.php
+++ smile/dashboard.php
@@ -26,22 +26,32 @@
         	<?php include('dashboard-sidebar.php');?>
         	
 	        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-	          	<h1 class="page-header">Your Dashboard:</h1>
-	          	<h3>You can either</h3>
+	          	<h1 class="page-header">Your Dashboard</h1>
+	          
+	            <h3>Quick guide</h3>
+	            <p><strong>Step1: </strong> Add a product into your catalogue</p>
+	            <a href ="product-add.php" title="Add Business" class="btn btn-primary btn-lg active" role="button">Add Product</a>
+	            <p></p>
+	            <p><strong>Step2: </strong> Add provenance information associated with the product involving production, recipies and ingredients</p>
+	            <a href ="product-list-all.php" title="Add Business" class="btn btn-primary btn-lg active" role="button">Add cards</a>
+	            <p></p>
+	            
+	            <p><strong>Step3: </strong> Create a new item</p>
+	            <a href ="item-add.php" title="Add Business" class="btn btn-primary btn-lg active" role="button">Add Item</a>
+	            <p></p>
+	            
+	            
+	            <p><strong>Step4: </strong> Print the QR code and attach it to your product</p>
+		         <a href ="item-list-all.php" title="Add Business" class="btn btn-primary btn-lg active" role="button">Print QR code</a>
+		         <p></p>
 
+                <h3>Other options</h3>
 	          	<a href ="register-user.php" title="Add User" class="btn btn-primary btn-lg active" role="button">Add a new user</a> &nbsp;&nbsp;
 	          	<?php if($_SESSION['account']['accountType'] === "Business Account"){ ?>
 	          		<a href ="register-business.php?bpid=<?php echo $id;?>" title="Add Business" class="btn btn-primary btn-lg active" role="button">Add a new business</a>
 	          		<br/><br/>
 	          	<?php } ?>
-	          	<h3>Or select an already existing business:</h3>
-	          	<?php
-	          		//print_r($_SESSION['account']['businessApiKeys']);
-	          		echo "<br />";
-	          		//echo $_SESSION['account']['currentBusinessKey'];
-	          		echo "<br />";
-	          		//echo $size;
-	          	 ?>
+	          	
 	          	
 	        </div><!--End of .main-->
 	    </div><!--End of .row-->
